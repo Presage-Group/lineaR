36cb1d9a73f7067dc14d065128a71e242f9e7a22
Randy Boyes <rboyes@presagegroup.com>
2025-01-15 13:55:29 -0500

    adds ability to interact with users

---
+++ R/users.R
@@ -0,0 +1,24 @@
+#' Returns a tibble with your id, name, and email. Useful for checking if you have successfully authenticated with the linear API.
+#'
+#' @return tibble
+#' @export
+me <- function(){
+  link <- 'https://api.linear.app/graphql'
+  conn <- ghql::GraphqlClient$new(url = link, headers = list("Authorization" = get_linear_api_key()))
+
+  qry <- ghql::Query$new()
+
+  qry$query("me", 
+  '{
+    viewer {
+      id
+      name
+      email
+    }
+  }')
+
+  res <- conn$exec(qry$queries$me)
+
+  result <- jsonlite::fromJSON(conn$exec(new$link), flatten = TRUE)
+  return(dplyr::as_tibble(result$data$viewer))
+}
--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,7 @@
+#' Stores your API key in your keyring. 
+#'
+#' @return nothing
+#' @export
 store_linear_api_key <- function(api_key){
   keyring::key_set_with_value("linear_api", password = api_key)
 }
