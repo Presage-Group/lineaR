d0b0e5137bf36e09d3eda6730fea6a4f8cb904ee
Randy Boyes <rboyes@presagegroup.com>
2025-01-24 14:36:51 -0500

    removes ghql dependency

--- R/issues.R
+++ R/issues.R
@@ -1,18 +1,13 @@
 #' Returns a tibble with the issues for a given team_id
 #'
 #' @param team_id The id for the team. Use get_linear_teams() to find the id of the team you want. 
+#' @param api_url Defaults to the current linear api url, but can be changed 
 #' 
 #' @return tibble
 #' @importFrom methods new
 #' @export
-get_linear_issues <- function(team_id){
-  link <- 'https://api.linear.app/graphql'
-  conn <- ghql::GraphqlClient$new(url = link, headers = list("Authorization" = get_linear_api_key()))
-
-  qry <- ghql::Query$new()
-
-  qry$query("issues", 
-  paste0('{
+get_linear_issues <- function(team_id, api_url = "https://api.linear.app/graphql"){
+  graphql_query <- paste0('{
   team(id: "',  team_id, '") {
     id
     name
@@ -31,12 +26,20 @@ get_linear_issues <- function(team_id){
       }
     }
   }
-  }'))
+  }')
 
-  res <- conn$exec(qry$queries$issues)
+  body <- list(query = graphql_query)
 
-  result <- jsonlite::fromJSON(res, flatten = TRUE)
-  return(dplyr::as_tibble(result$data$team$issues$nodes))
+  response <- httr2::request(api_url) |> 
+    httr2::req_headers(
+      "Authorization" = get_linear_api_key(),
+      "Content-Type" = "application/json"
+    ) |> 
+    httr2::req_body_json(body) |> 
+    httr2::req_perform()
+
+  response_body <- httr2::resp_body_json(response)
+  return(dplyr::as_tibble(response_body))
 }
 
 #' Creates a new issue within a team specified using the team's id. 
@@ -44,11 +47,16 @@ get_linear_issues <- function(team_id){
 #' @param title The issue title
 #' @param description The issue description
 #' @param team_id The id for the team. Use get_linear_teams() to find the id of the team you want. 
+#' @param api_url Defaults to the current linear api url, but can be changed 
 #' 
 #' @return tibble
-#' @importFrom methods new
 #' @export
-create_linear_issue <- function(title, description, team_id){
+create_linear_issue <- function(
+  title, 
+  description, 
+  team_id, 
+  api_url = "https://api.linear.app/graphql"){
+  
   graphql_query <- paste0('mutation {
     issueCreate(input: {
       title: "', title, '",
@@ -64,7 +72,7 @@ create_linear_issue <- function(title, description, team_id){
 
   body <- list(query = graphql_query)
 
-  response <- httr2::request("https://api.linear.app/graphql") |> 
+  response <- httr2::request() |> 
     httr2::req_headers(
       "Authorization" = get_linear_api_key(),
       "Content-Type" = "application/json"
